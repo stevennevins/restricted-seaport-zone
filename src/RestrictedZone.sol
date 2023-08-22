6a4eaca063eab126927bdfcebcf60fb54b09ef75
steven nevins <steven.nevins.js@gmail.com>
2023-08-22 10:22:50 -0400

    wip

--- src/RestrictedZone.sol
+++ src/RestrictedZone.sol
@@ -1,40 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ZoneInteractionErrors} from "seaport-types/interfaces/ZoneInteractionErrors.sol";
 import {ZoneInterface} from "seaport-types/interfaces/ZoneInterface.sol";
 import {SeaportInterface} from "seaport-types/interfaces/SeaportInterface.sol";
-import {Order, Schema, ZoneParameters} from "seaport-types/lib/ConsiderationStructs.sol";
+import {Order, Schema, SpentItem, ReceivedItem, ZoneParameters} from "seaport-types/lib/ConsiderationStructs.sol";
 
 import {OwnableUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 import {PausableUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/security/PausableUpgradeable.sol";
+import {ERC165Upgradeable} from "openzeppelin-contracts-upgradeable/contracts/utils/introspection/ERC165Upgradeable.sol";
 
-/// @notice A seaport zone that restricts the fulfillment of orders to have originated through the entrypoint on the zone
-contract RestrictedZone is PausableUpgradeable, OwnableUpgradeable, ZoneInteractionErrors, ZoneInterface {
-    address internal seaport;
-    uint96 private mutex = 1;
+contract PausableZone is PausableUpgradeable, OwnableUpgradeable, ERC165Upgradeable, ZoneInterface {
+    event OrderFilled(
+        address indexed fulfiller,
+        address indexed offerer,
+        SpentItem[] offer,
+        ReceivedItem[] consideration,
+        bytes32[] orderHashes
+    );
 
-    modifier lock() {
-        require(mutex == 1, "Locked");
-        mutex = 2;
-        _;
-        mutex = 1;
-    }
-
-    function initalize(address _seaport) external initializer {
-        seaport = _seaport;
+    function initalize() external initializer {
         __Ownable_init();
         __Pausable_init();
     }
 
-    function fulFillOrder(Order calldata order, bytes32 _conduitKey) external payable whenNotPaused lock {
-        SeaportInterface(seaport).fulfillOrder{value: msg.value}(order, _conduitKey);
-    }
-
     /* @notice Pause this contract, safely stopping orders from using
      *         the contract as a zone. Restricted orders with this address as a
-     *         zone will not be fulfillable unless the zone is redeployed to the
-     *         same address.
+     *         zone will not be fulfillable unless the zone is unpaused. 
      */
     function pause() external onlyOwner {
         _pause();
@@ -59,20 +50,28 @@ contract RestrictedZone is PausableUpgradeable, OwnableUpgradeable, ZoneInteract
      * @return validOrderMagicValue A magic value indicating if the order is
      *                              currently valid.
      */
-    function validateOrder(ZoneParameters calldata) external view returns (bytes4) {
-        // Return the selector of isValidOrder as the magic value.
-        if (mutex == 2 && !paused()) return ZoneInterface.validateOrder.selector;
-        return bytes4(0);
+    function validateOrder(ZoneParameters calldata params) external returns (bytes4) {
+        if (paused()) {
+            return bytes4(0);
+        }
+
+        emit OrderFilled(params.fulfiller, params.offerer, params.offer, params.consideration, params.orderHashes);
+        return ZoneInterface.validateOrder.selector;
     }
 
     /**
      * @dev Returns the metadata for this zone.
      */
     function getSeaportMetadata() external pure returns (string memory name, Schema[] memory schemas) {
-        return ("RestrictedZone", schemas);
+        return ("PausableZone", schemas);
     }
 
-    function supportsInterface(bytes4) external pure returns (bool) {
-        return true;
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC165Upgradeable, ZoneInterface)
+        returns (bool)
+    {
+        return interfaceId == type(ZoneInterface).interfaceId || super.supportsInterface(interfaceId);
     }
 }
